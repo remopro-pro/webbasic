4d2426ca565718dff974b4356b9504af4ce66e61
hirfujit <hirfujit@yahoo-corp.jp>
2020-07-21 19:40:53 +0900

    fix

--- 01_blog_quick_start/0_start_python/5_function/func1.py
+++ 01_blog_quick_start/0_start_python/5_function/func1.py
@@ -5,12 +5,12 @@ def add(a, b):
 # 用語の説明: ↑のaddを「関数」、a, bを「引数」、returnで返ってくる値を「戻り値」という
 
 
-def welcome_message(name):
+def get_welcome_message(name):
     return "ようこそ " + name + "さん"
 
 
 result = add(1, 2)
 print(result)
 
-msg = welcome_message("John")
+msg = get_welcome_message("John")
 print(msg)
